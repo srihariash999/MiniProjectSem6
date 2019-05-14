7586349ed9f0830de6b50ad496b14f37460f639b
Srihari Ayapilla <16bec021@smvdu.ac.in>
2019-05-14 22:38:00 +0530

    Serial test

--- test/test.ino
+++ test/test.ino
@@ -1,66 +1,37 @@
+
+
 #include<EEPROM.h>
+#include <LiquidCrystal.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266HTTPClient.h>
 
 
-void setup() {
-  
-  EEPROM.begin(512);
-  Serial.begin(115200);
+LiquidCrystal lcd(D5, D6, D3, D2, D1, D0);
 
+void setup()
+{
+    pinMode(D4,OUTPUT);
+  analogWrite(D4, 200);
+  lcd.begin(16, 2);
+   attachInterrupt(digitalPinToInterrupt(D7), IntCallback, FALLING);
+   Serial.begin(115200);
 }
 
-int lenaddr = 99;
-int ssidaddr = 100;
-
-//void loop() {
-//
-//
-//String p = {};
-//int len = EEPROM.read(lenaddr);
-//Serial.print("Length is = ");
-//Serial.println(len);
-//
-//
-//for (int i =0; i<len; i++)
-//{char n = EEPROM.read(ssidaddr+i);
-//Serial.println(n);
-//  p+=n ;
-//}
-//
-//Serial.print("Complete String is :");
-//Serial.println(p);
-//Serial.println(p.length());
-//p.trim();
-//Serial.print(p);
-//Serial.print("  with length after trim :");
-//Serial.println(p.length());
-//delay(2000);
-//
-//}
-
-
+String p;
 void loop()
 {
- while(!Serial.available());
-String p = Serial.readString();
-EEPROM.write(lenaddr,p.length());
-EEPROM.commit();
-char buff[p.length()];
-for (int i =0; i<p.length(); i++)
-{
-  buff[i] = p[i];  
-}
 
-Serial.println(p.length());
-for (int i =0; i<p.length(); i++)
-{
-  Serial.print(buff[i]);
-  EEPROM.write(ssidaddr+i,buff[i]);
-  EEPROM.commit();
+  while(Serial.available())
+  {
+    p+= Serial.read();
+  }
   
 }
 
 
+void IntCallback()
+{
 
-
-
+   Serial.println(p);
 }
